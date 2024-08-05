6481a1f9b4a6377c2595b1258d215e463e86de66
ALi <92468722+muhammadali009x@users.noreply.github.com>
2024-08-05 08:11:20 +0500

    first comment

---
+++ pages/_app.js
@@ -0,0 +1,98 @@
+import PreLoader from "@/src/layout/PreLoader";
+import "@/styles/globals.css";
+import Head from "next/head";
+import { Fragment, useEffect, useState } from "react";
+import niceSelect from "react-nice-select";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      niceSelect();
+    }, 500);
+  });
+
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1500);
+  }, []);
+
+  return (
+    <Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {/* Title */}
+        <title>Ygency - Web Agency React NextJS Template</title>
+        {/* Favicon Icon */}
+        <link
+          rel="shortcut icon"
+          href="assets/images/favicon.png"
+          type="image/x-icon"
+        />
+        {/* Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Flaticon */}
+        <link rel="stylesheet" href="assets/css/flaticon.min.css" />
+        {/* Font Awesome */}
+        <link rel="stylesheet" href="assets/css/fontawesome-5.14.0.min.css" />
+        {/* Bootstrap */}
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        {/* Magnific Popup */}
+        <link rel="stylesheet" href="assets/css/magnific-popup.min.css" />
+        {/* Nice Select */}
+        <link rel="stylesheet" href="assets/css/nice-select.min.css" />
+        {/* Animate */}
+        <link rel="stylesheet" href="assets/css/animate.min.css" />
+        {/* Slick */}
+        <link rel="stylesheet" href="assets/css/slick.min.css" />
+        {/* Main Style */}
+        <link rel="stylesheet" href="assets/css/style.css" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      {!loaded && <PreLoader />}
+      {loaded && <Component {...pageProps} />}
+    </Fragment>
+  );
+}
